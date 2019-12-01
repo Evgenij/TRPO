2664d587533518631d04418d767499a470d55fff
Evgenij <evgenij.ermolenko@list.ru>
2019-12-01 15:58:22 +0300

    rename function

--- 9.cs
+++ 9.cs
@@ -2,7 +2,7 @@ return ((int)(Counter / 2) != Counter / 2.00 && Counter != 0);
 
 //----------------------------------------------------------------------
 
-bool method9(int Counter)
+bool division(int Counter)
 {
     bool f1 = Counter / 2.00 && Counter != 0;
     bool f2 = (int)(Counter / 2) != f1;
